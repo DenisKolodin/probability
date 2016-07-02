41a0d2c578d40708a443834be6a0fe74170b7385
Travis CI <>
2016-07-02 14:18:59 +0000

    Update the documentation

---
+++ probability/prelude/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Bernoulli","A Bernoulli distribution."],["Beta","A beta distribution."],["Binomial","A binomial distribution."],["Categorical","A categorical distribution."],["Exponential","An exponential distribution."],["Gamma","A gamma distribution."],["Gaussian","A Gaussian distribution."],["Uniform","A continuous uniform distribution."]]});
